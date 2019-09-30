48b0eedd89be7c17af0616087fab3e7d4965cf86
abhinavtrivedi1 <officialronuabhinav@gmail.com>
2019-10-01 01:06:02 +0530

    abhinav

---
+++ src/main/java/com/thoughtmechanix/licenses/controllers/License.java
@@ -0,0 +1,4 @@
+package com.thoughtmechanix.licenses.controllers;
+
+public class License {
+}
---
+++ src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -0,0 +1,7 @@
+package com.thoughtmechanix.licenses.controllers;
+
+@RestController
+@RequestMapping(value="/v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+//Body of the class removed for conciseness
+}
